6ef6d982bf351a74c5d684f9246cc7774e8a1640
sheva29 <design@sanchezmauricio.com>
2017-04-20 14:37:44 -0400

    changing logic to eval_poly, making it more dynamic

--- unit1_6_recursion.py
+++ unit1_6_recursion.py
@@ -1,87 +1,87 @@
-# def keySearch(L, k):
-#     for elem in L:
-#         if elem[0] == k: return elem[1]
-#     return None
-#
-# EtoF = {'bread': 'du pain', 'wine': 'du vin',\
-#         'eats': 'mange', 'drinks': 'bois',\
-#         'likes': 'aime', 1: 'un',\
-#         '6.00':'6.00'}
-#
-# def translateWord(word, dictionary):
-#     if word in dictionary:
-#         return dictionary[word]
-#     else:
-#         return word
-#
-# def translate(sentence):
-#     translation = ''
-#     word = ''
-#     for c in sentence:
-#         if c != ' ': # when c isn't a space
-#             word = word + c # I build a word
-#         else: # when I find a space
-#             translation = translation + ' '\
-#                           + translateWord(word, EtoF) # we translate
-#             word = ''
-#     return translation[1:] + ' ' + translateWord(word, EtoF) #we pass the last word assuming it isn't a space
-#
+def keySearch(L, k):
+    for elem in L:
+        if elem[0] == k: return elem[1]
+    return None
+
+EtoF = {'bread': 'du pain', 'wine': 'du vin',\
+        'eats': 'mange', 'drinks': 'bois',\
+        'likes': 'aime', 1: 'un',\
+        '6.00':'6.00'}
+
+def translateWord(word, dictionary):
+    if word in dictionary:
+        return dictionary[word]
+    else:
+        return word
+
+def translate(sentence):
+    translation = ''
+    word = ''
+    for c in sentence:
+        if c != ' ': # when c isn't a space
+            word = word + c # I build a word
+        else: # when I find a space
+            translation = translation + ' '\
+                          + translateWord(word, EtoF) # we translate
+            word = ''
+    return translation[1:] + ' ' + translateWord(word, EtoF) #we pass the last word assuming it isn't a space
+
 # print(translate('John eats bread'))
 # print(translate('Eric drinks wine'))
 # print(translate('Everyone likes 6.00'))
 #
-# def toChars(s):
-#     # import string
-#     s = s.lower()
-#     ans = ''
-#     for c in s:
-#         if c.islower():
-#             ans = ans + c
-#     return ans
-#
-# def isPal(s):
-#     if len(s) <= 1: # if the middle is an odd number
-#         return True
-#     else:
-#         # print("comparing: " + s[0] + " - " + s[-1])
-#         return s[0] == s[-1] and isPal(s[1:-1])
-#
-# def isPalindrome(s):
-#     """Returns True if s is a palindrome and False otherwise"""
-#     return isPal(toChars(s))
-#
+def toChars(s):
+    # import string
+    s = s.lower()
+    ans = ''
+    for c in s:
+        if c.islower():
+            ans = ans + c
+    return ans
+
+def isPal(s):
+    if len(s) <= 1: # if the middle is an odd number
+        return True
+    else:
+        # print("comparing: " + s[0] + " - " + s[-1])
+        return s[0] == s[-1] and isPal(s[1:-1])
+
+def isPalindrome(s):
+    """Returns True if s is a palindrome and False otherwise"""
+    return isPal(toChars(s))
+
 # # print(isPalindrome('Guttag'))
 # # print(isPalindrome('Guttug'))
 # # print(isPalindrome('Able was I ere I saw Elba'))
 # print(isPalindrome('Are we not drawn onward, we few, drawn onward to new era?'))
 #
-# def fib(x):
-#     """assumes x an int >= 0
-#         Returns Fibonacci of x"""
-#     assert type(x) == int and x >=  0
-#     if x == 0 or x == 1:
-#         return 1
-#     else:
-#         return fib(x-1) + fib(x-2)
-#
-# def testFib(n):
-#     for i in range(n+1):
-#         print ('fib of', i, '=', fib(i))
-#
-#
-# def simpleExp(b, n):
-#     if n == 0:
-#         return 1
-#     else:
-#         return b * simpleExp(b, n-1)
-#
-# def Hanoi(n, f, t, s):
-#     if n == 1:
-#         print("move from", f, "to", t)
-#     else:
-#         Hanoi( n-1, f, s, t)
-#         Hanoi(1, f, t, s )
-#         Hanoi(n-1, s, t, f)
+def fib(x):
+    """assumes x an int >= 0
+        Returns Fibonacci of x"""
+    assert type(x) == int and x >=  0
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return fib(x-1) + fib(x-2)
+
+def testFib(n):
+    for i in range(n+1):
+        print ('fib of', i, '=', fib(i))
+
+
+def simpleExp(b, n):
+    if n == 0:
+        return 1
+    else:
+        return b * simpleExp(b, n-1)
+
+def Hanoi(n, f, t, s):
+    if n == 1:
+        print("move from", f, "to", t)
+    else:
+        Hanoi( n-1, f, s, t)
+        Hanoi(1, f, t, s )
+        Hanoi(n-1, s, t, f)
 
 #print(Hanoi(5, "f", "t", "s"))
 
@@ -109,12 +109,14 @@ def Exponent(numerator, exponent):
         return numerator * Exponent(numerator, exponent -1)
 
 def evaluate_poly(poly, x):
-    return   float((poly[2] * Exponent(x,4)) + (poly[1] * Exponent(x,3)) + (poly[0] * Exponent(x,2)))
+    result = 0
+    for i in range(len(poly)):
+        result += float(poly[i] * Exponent(x, i))
+    return result
 
-
-poly = (5.0, 9.3, 7.0)
+poly = (0.0, 0.0, 5.0, 9.3, 7.0)
 x = -13
-
+# print(evaluate_poly(poly, x))
 #
 # Problem 2
 #
@@ -136,7 +138,7 @@ def Derive(poly, derivative, index):
         return derivative
     else:
         derivative = derivative + (float( index * poly[index]), )
-        return Derive(poly, derivative, index + 1)
+        return Derive(poly, derivative, index + 1) # **# RETURN as a key word is neccesarry in order to bubble up the bottom function
 
 def compute_deriv(poly):
     derivative = ()
@@ -145,7 +147,33 @@ def compute_deriv(poly):
 
 poly = (-13.39, 0.0, 17.5, 3.0, 1.0)
 
+#
+# Problem 3
+#
+# Uses Newton's method to find and return a root of a polynomial function.
+# Returns a tuple containing the root and the number of iterations required
+# to get to the root.
+#
+# Example:
+# >>> poly = (-13.39, 0.0, 17.5, 3.0, 1.0)    #x^4 + 3x^3 + 17.5x^2 - 13.39
+# >>> x_0 = 0.1
+# >>> epsilon = .0001
+# >>> print compute_root(poly, x_0, epsilon)
+# (0.80679075379635201, 8.0)
+#
+# poly: tuple of numbers, length > 1.
+#      Represents a polynomial function containing at least one real root.
+#      The derivative of this polynomial function at x_0 is not 0.
+# x_0: float
+# epsilon: float > 0
+# returns: tuple (float, int)
+# def evaluate_derivative()
+# def compute_root(poly, x_0, epsilon):
+#     f = evaluate_poly(poly, x_0)
+#     fprime = compute_deriv(poly)
+    # TO DO ...
+
 
 # print(Exponent(2, 6))
 # print(evaluate_poly(poly, x))
-print("derivative: ", compute_deriv(poly))
+# print("derivative: ", compute_deriv(poly))
